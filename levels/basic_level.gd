af868ce1229a3c5b35c67380fea5da693f5dae6b
AuraDvin <edvin.garibovic@gmail.com>
2026-01-03 15:12:39 +0100

    basic level

---
+++ levels/basic_level.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+var cooldown = 0.5
+var local_cooldown = 0
+
+# Todo 
+var tower = preload("res://characters/towers/test_tower/test_tower.tscn")
+var enemy = preload("res://characters/enemies/test_enemy/test_enemy.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$EnemySpawnTimer.start(RandomNumberGenerator.new().randf()* 3)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	local_cooldown -= delta
+
+
+func _on_grid_clicked_on_grid(tile_position, tile_size):
+	if local_cooldown > 0:
+		return
+	var new_tower = tower.instantiate()
+	$towers.add_child(new_tower)
+	new_tower.position += tile_position * tile_size
+	new_tower.position += tile_size / 5
+	local_cooldown = cooldown
+
+
+func _on_enemy_spawn_timer_timeout():
+	var rng = RandomNumberGenerator.new()
+	var lane = rng.randi_range(0, 4)
+	var enemy_inst = enemy.instantiate()
+	$Lanes.get_child(lane).add_child(enemy_inst)
+	$EnemySpawnTimer.start(rng.randf()* 3 + 2)
