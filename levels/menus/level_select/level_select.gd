c4ffc9503c5197d6ae869d24a6921594639c2ea0
aura <edvin.garibovic@gmail.com>
2026-01-06 21:23:25 +0100

    initial menus

---
+++ levels/menus/level_select/level_select.gd
@@ -0,0 +1,3 @@
+class_name LevelSelect
+
+extends Control
---
+++ levels/menus/main_menu/main_menu.gd
@@ -0,0 +1,37 @@
+class_name MainMenu
+extends Control
+
+
+# todo:
+var level_selct_scene = preload("res://levels/basic_level/basic_level.tscn").instantiate()
+# var level_selct_scene = preload("res://levels/menus/level_select/level_select.tscn").instantiate()
+
+# todo:
+var settings_scene = null
+	
+# Icons are currently from the following sources: 
+# Settings:
+# https://www.flaticon.com/free-icon/setting_2040504?term=settings&related_id=2040504
+
+# Play:
+# https://www.flaticon.com/free-icon/play-button-arrowhead_27223?term=play&page=1&position=1&origin=search&related_id=27223
+
+# Quit:
+# https://www.flaticon.com/free-icon/logout_1828479?term=exit&page=1&position=5&origin=search&related_id=1828479
+
+func _on_quit_confirmation_confirmed() -> void:
+	get_tree().quit(0)
+	
+
+func _on_quit_btn_pressed() -> void:
+	$QuitConfirmation.visible = true
+	
+
+func _on_play_btn_pressed() -> void:
+	get_tree().root.add_child(level_selct_scene)
+	get_tree().unload_current_scene()
+
+
+func _on_settings_btn_pressed() -> void:
+	get_tree().root.add_child(settings_scene)
+	get_tree().unload_current_scene()
