23e5ff74118fa53f02d65b4eb9db6e387706c176
aura <edvin.garibovic@gmail.com>
2026-01-07 18:30:07 +0100

    type hints and return types
    just because

--- misc/scene_switcher.gd
+++ misc/scene_switcher.gd
@@ -9,20 +9,20 @@ func _ready() -> void:
 	scene_history.append(current_scene)
 	print_debug(current_scene)
 
-func switchScene(res_path): 
+func switchScene(res_path: String) -> void: 
 	call_deferred("defferedSwitchScene", res_path)
 
-func defferedSwitchScene(res_path: String):
+func defferedSwitchScene(res_path: String) -> void:
 	current_scene.free() 
 	var s = load(res_path)
 	current_scene = s.instantiate()
 	scene_history.append(res_path) 
 	get_tree().root.add_child(current_scene)
 
-func returnToPrevScene(): 
+func returnToPrevScene() -> void: 
 	scene_history.pop_back() # current scene
 	var s = scene_history.pop_back() # last scene
-	
+	# Fallback to main_menu scene
 	if s == null: 
 		s = "res://levels/menus/main_menu/main_menu.tscn"
 	switchScene(s)
