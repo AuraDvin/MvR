48b8b5b9b133d5c1712e7bfb38b71a6cf41299c3
uselesnik <62332372+uselesnik@users.noreply.github.com>
2026-01-09 21:55:29 +0100

    created upgrade GUI

--- levels/menus/hud/hud.gd
+++ levels/menus/hud/hud.gd
@@ -7,6 +7,10 @@ var selected_upgrades
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	await get_tree().process_frame
+	for child in $MarginContainer/HBoxContainer2/VBoxContainer2/upgrades.get_children():
+		if child.name == "Label":
+			continue
+		child.pressed.connect(_on_upgrade_pressed.bind(child.get_index() - 1))
 	for child in $MarginContainer/HBoxContainer2/VBoxContainer/shopContainer.get_children():
 		child.pressed.connect(_on_button_pressed.bind(child.get_index() as tower_type))
 		var tb = child as TextureButton
@@ -36,8 +40,16 @@ func _show_upgrades(node):
 	var upgrades = $MarginContainer/HBoxContainer2/VBoxContainer2/upgrades
 	upgrades.visible = true
 	selected_upgrades = node
+	update_costs(node)
+
+func update_costs(node):
+	var upgrades = $MarginContainer/HBoxContainer2/VBoxContainer2/upgrades
 	for i in range(3):
-		print(node.type, i, node.bought_upgrades[i])
+		if node.bought_upgrades[i] >= UpgradeManager.upgrade_costs[node.type][i].size():
+			upgrades.get_child(i + 1).text = "sold out"
+			upgrades.get_child(i + 1).disabled = true
+			continue
+		upgrades.get_child(i + 1).disabled = false
 		var line2 = "\nenergy required: " + str(UpgradeManager.upgrade_costs[node.type][i][node.bought_upgrades[i]]) 
 		upgrades.get_child(i + 1).text = UpgradeManager.upgrade_names[node.type][i] + line2 
 
@@ -45,3 +57,15 @@ func _hide_upgrades():
 	$MarginContainer/HBoxContainer2/VBoxContainer2/upgrades.visible = false
 	selected_upgrades = null
 	$Selector.visible = false
+
+func _on_upgrade_pressed(i: int):
+	var player = $"../Player"
+	if selected_upgrades == null:
+		return
+	var current_upgrade = selected_upgrades.bought_upgrades[i]
+	var energy = player.spend_energy(UpgradeManager.upgrade_costs[selected_upgrades.type][i][selected_upgrades.bought_upgrades[i]])
+	if energy == -1:
+		print_debug("premalo energije")
+		return
+	selected_upgrades.bought_upgrades[i] += 1
+	update_costs(selected_upgrades)
