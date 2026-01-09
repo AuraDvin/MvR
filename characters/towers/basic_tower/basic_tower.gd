f680a9f15398373fbee152bf1226e9868decd50b
Barrel <jj93378@gmail.com>
2026-01-09 20:12:50 +0100

    fix basic turret _ready()

--- characters/towers/basic_tower/basic_tower.gd
+++ characters/towers/basic_tower/basic_tower.gd
@@ -10,8 +10,7 @@ func _init():
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super()
-	attack_timer.start()
-	super()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
