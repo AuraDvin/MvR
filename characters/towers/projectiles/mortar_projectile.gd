9ea647a3b839c43d3ca8eaf1629a14c4e97765a0
uselesnik <62332372+uselesnik@users.noreply.github.com>
2026-01-10 18:56:05 +0100

    mortar projectile particles

--- characters/towers/projectiles/mortar_projectile.gd
+++ characters/towers/projectiles/mortar_projectile.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var targetEnemy :Node2D
 var damage: int = 3
-
+const MORTAR_PROJECTILE_PARTICLE = preload("uid://cyxqmbusyi2dq")
 @export var initial_y_velocity: float = 25
 var speed: float = INF
 var velocity = Vector2.ONE
@@ -31,8 +31,12 @@ func _physics_process(delta):
 	velocity.y += delta * 50
 	position += velocity
 	if localTimer.is_stopped() and abs(position.y - initial_y) < 10:
+		var particle_ist = MORTAR_PROJECTILE_PARTICLE.instantiate()
+		particle_ist.position = global_position
+		$"../../particles".add_child(particle_ist)
 		for i in $Area2D.get_overlapping_areas():
 			if i.is_in_group("enemy_body"):
 				i.get_parent()._cause_damage(2)
 		queue_free()
+		
 	
