b268dc2ba7e504ca61d690185a6341c725ecadd5
aura <edvin.garibovic@gmail.com>
2026-01-11 12:53:12 +0100

    player ref and updating energy
    loading level data will now give the right energy levels

--- levels/basic_level/basic_level.gd
+++ levels/basic_level/basic_level.gd
@@ -10,6 +10,7 @@ signal energy_changed(newAmount: int)
 @onready var pause_popout = $PausePopout
 @onready var soundtrack: AudioStreamPlayer2D = $"../Soundtrack"
 @onready var wave_spawn_timer = $EnemySpawnTimer
+@onready var player = $"../Player"
 
 var lane_count = 5
 var cooldown = 0.5
@@ -42,20 +43,17 @@ func _ready():
 	if not LevelDataManager.load_state(self):
 		# Set by Level Selector item button
 		var data = LevelDataManager.get_data(LevelDataManager.current_level_name)
-		if data.energy == TYPE_INT:
-			$"../Player".energy=data.energy
-		$"../Player".energy=data.energy
+		# Set the max score of the level
 		for es in data.enemy_queue:
 			for e in es: 
-				# Get the amount of score for each enemy
-				# print(e.type, " this many times ->", e.count)
 				var tmp_enemy = enemies[(int)(e.type)].instantiate()
 				data.max_score += tmp_enemy.score
 				tmp_enemy.queue_free()
-		print("Max level score is: ", data.max_score)
+#		print("Max level score is: ", data.max_score)
 		LevelDataManager.current_level_data = data
+		player.energy=data.energy
 		# Spawn first wave (also updates the timer string)
-		$EnemySpawnTimer.emit_signal("timeout")
+		$EnemySpawnTimer.start(5)
 	else: 
 		for tower: Tower in grid_towers.get_children():
 			var tower_pos = Vector2(tower.x, tower.y)
@@ -63,23 +61,21 @@ func _ready():
 		for lane in lanes.get_children():
 			for enemy: Enemy in lane.get_children(): 
 				enemy.defeated.connect(update_score)
-#		print_debug("Printing with time", LevelDataManager.current_level_data.wave_time_remaining)
 		# use the wait time that was loaded from level data
 		$EnemySpawnTimer.start(LevelDataManager.current_level_data.wave_time_remaining) 
 		$ChangeTimerLabel.emit_signal("timeout") # immediatelly update timer string
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	local_cooldown -= delta
-	# print_debug(timer_text)
-	# todo: Set the label.text = timer_text (if different?)
+	# This is the strin that should be applied to a label's text
+#	 print_debug(timer_text)
 	if stop_spawning: 
 		var iii = 0
 		for lane1 in lanes.get_children():
-			# print("count", lane1.get_child_count())
+#			print("count", lane1.get_child_count())
 			if lane1.get_child_count() == 0: 
 				iii += 1
-		# print("final count", iii)
+#		print("final count", iii)
 		if iii >= lane_count: 
 			$AcceptDialog.visible = true
 			timer_text = "Game Over!"
@@ -93,7 +89,6 @@ func _input(event: InputEvent) -> void:
 		pause_popout.visible = not pause_popout.visible
 
 func _on_grid_clicked_on_grid(tile_position, tile_size):
-	var player = $"../Player"
 	var energy
 	if player.holding == player.hand.NONE:
 		var selected = grid_spaces.get(tile_position) 
@@ -192,12 +187,11 @@ func _get_enemy_spawn_position(lane) -> Vector2:
 	return Vector2(x_position, y_position)
 
 func _mode_selected(node):
-	$"../Player"._set_hand(node)
-	if node == $"../Player".hand.NONE:
+	player._set_hand(node)
+	if node == player.hand.NONE:
 		empty_selection()
 
 func empty_selection():
-	var player = $"../Player"
 	player.holding = player.hand.NONE
 	$"../Hud"._hide_upgrades()
 
